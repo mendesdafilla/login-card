e9924c2662401e693af4e3a253c9c4405a44f598
mendesdafilla <dafila.mendes@alunoescolassociais.org.br>
2024-08-09 08:17:33 -0300

    adiciona readme

--- controller.js
+++ controller.js
@@ -1,27 +1,48 @@
-let emailArray = [];
 
-function checarEmail() {
-    const emailField = document.forms[0].email;
-    const email = emailField.value;
+// FUNÇÃO PARA VALIDAÇÃO DE ACESSO
+ 
+function acessar(){
+   let loginEmail = document.getElementById('loginEmail').value;
+   let loginSenha = document.getElementById('loginSenha').value;
 
-    if (email === "" || email.indexOf("@") === -1 || email.indexOf(".") === -1) {
-        alert("Por favor, informe um email válido");
-        return false;
-    } else {
-        alert("Email informado");
-        emailArray.push(email);
-        atualizarEmailList();
-        emailField.value = "";  // Limpa o campo de email após adicionar
-        return false;  // Impede o envio do formulário para que a página não seja recarregada
-    }
+   if(!loginEmail || !loginSenha){
+       alert("Favor preencher todos os campos");
+   }else{
+       //alert("Campos preenchidos com sucesso");
+       window.location.href = 'cadastro.html';
+   }
 }
 
-function atualizarEmailList() {
-    const emailList = document.getElementById("emailList");
-    emailList.innerHTML = "";
-    emailArray.forEach(email => {
-        const li = document.createElement("li");
-        li.textContent = email;
-        emailList.appendChild(li);
-    });
+// FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+var dadosLista = [];
+
+function salvarUser(){
+   let nomeUser = document.getElementById('nomeUser').value;
+   if(nomeUser){
+       dadosLista.push(nomeUser);
+       //console.log(dadosLista);
+       criaLista();
+       document.getElementById('nomeUser').value = "";
+   }else{
+       alert("Favor informe um nome para cadastro");
+   }
+}
+// FUNÇÃO PARA CRIAR LISTA DE USUÁRIOS
+function criaLista(){
+   let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+   for(let i=0;i<=(dadosLista.length - 1);i++){
+       tabela += "<tr><td>" + dadosLista[i] + "</td><td><button type='button' onclick='editar(parentNode.parentNode.rowIndex)'>Editar</button><button type='button' onclick='excluir(parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+       document.getElementById('tabela').innerHTML = tabela;
+   }
+}
+
+// FUNÇÃO PARA EDITAR NOMES DE LISTA
+function editar(i){
+   document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+   dadosLista.splice(dadosLista[(i - 1)], 1);
+}
+// FUNÇÃO PARA EXCLUIR NOME DE LISTA
+function excluir(i){
+   dadosLista.splice((i - 1), 1);
+   document.getElementById('tabela').deleteRow(i);
 }
